663b0b7ffdbb74ea63f0810b4458d42c59fb2e2a
ewbidd <abidd166@gmail.com>
2025-04-19 23:55:57 +0700

    nineth

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-// Typing Effect
-const texts = ["Developer", "Problem Solver", "Tech Explorer"];
-let count = 0, index = 0, currentText = '', letter = '';
+const texts = ["Developer", "Tech Enthusiast", "Student"];
+let count = 0;
+let index = 0;
+let isDeleting = false;
+let currentText = "";
+let letter = "";
 
 (function type() {
-  if (count === texts.length) count = 0;
   currentText = texts[count];
-  letter = currentText.slice(0, ++index);
-  document.getElementById('typed-text').innerHTML = `Hello, I'm a <span class="text-primary">${letter}</span>`;
-  if (letter.length === currentText.length) {
-    count++; index = 0;
-    setTimeout(type, 1500);
+  
+  if (isDeleting) {
+    letter = currentText.slice(0, --index);
   } else {
-    setTimeout(type, 100);
+    letter = currentText.slice(0, ++index);
   }
+
+  document.getElementById("typed-text").innerHTML = `Hello, I'm a <span class="text-primary">${letter}</span>`;
+
+  let delay = isDeleting ? 60 : 100;
+
+  if (!isDeleting && letter.length === currentText.length) {
+    isDeleting = true;
+    delay = 1500;
+  } else if (isDeleting && letter.length === 0) {
+    isDeleting = false;
+    count = (count + 1) % texts.length;
+    delay = 500;
+  }
+
+  setTimeout(type, delay);
 })();
